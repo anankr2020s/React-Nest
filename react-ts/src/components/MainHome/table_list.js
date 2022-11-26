62969b14efe3f197e16b6f1a2538df2f2ad092ca
pechkr2020 <pechjie3238@gmail.com>
2022-11-26 14:25:19 +0700

    Add Delete Feature

--- react-ts/src/components/MainHome/table_list.js
+++ react-ts/src/components/MainHome/table_list.js
@@ -1,41 +1,74 @@
 import React, { useEffect, useState } from 'react';
-import {Table} from 'antd';
+import { Button, Space, Table } from 'antd';
+import { CloseOutlined } from '@ant-design/icons'
 import axios from 'axios';
-const { Column, ColumnGroup} = Table;
+const { Column, ColumnGroup } = Table;
 
 const TableList = () => {
-  const [data,SetData] = useState([]);
-  const ApiUrl = 'http://localhost:5000/user/ListUser'
+  const [data, SetData] = useState([]);
+  const Column = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id'
+    },
+    {
+      title: 'Username',
+      dataIndex: 'username',
+      key: 'username'
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email'
+    },
+    {
+      title: 'Password',
+      dataIndex: 'password',
+      key: 'password'
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size='middle'>
+          <Button danger>
+            <CloseOutlined onClick={()=>{
+              DeleteUser(record.id)
+            }} />
+          </Button>
+        </Space>
+      )
+    },
+  ]
+  const ApiUrl = 'http://localhost:5000/user'
   console.log(data)
-  useEffect(()=>{
+  useEffect(() => {
     GetUser()
-  },[])
-  function GetUser(){
-    axios.get(ApiUrl).then((response)=>{
-     console.log(response.data)
-     SetData(response.data)
+  }, [])
+  function GetUser() {
+    axios.get(`${ApiUrl}/ListUser`).then((response) => {
+      console.log(response.data)
+      SetData(response.data)
     })
-    .catch((error)=>{
-     console.log("err: ",error)
-    })
-   }
+      .catch((error) => {
+        console.log("err: ", error)
+      })
+  }
 
-  function DeleteUser(userid){
-    axios.delete()
+  function DeleteUser(userid) {
+    axios.delete(`${ApiUrl}/DeleteUser/`+userid).then((response)=>{
+      console.log(response.data)
+      GetUser()
+    }).catch((error)=>{
+      console.log("err:",error)
+    })
   }
   return (
     <div>
-      <Table dataSource={data}>
-          <ColumnGroup title='User Data'>
-          <Column title = 'UserID' dataIndex='id' key='id'/>
-          <Column title = 'Username' dataIndex='username' key='username'/>
-          <Column title = 'Email' dataIndex='email' key='email'/>
-          <Column title = 'Password' dataIndex='password' key='password'/>
-          </ColumnGroup>
-      </Table>
-      
+      <Table dataSource={data} columns={Column} />
     </div>
-   
+
   );
 };
 export default TableList;
